1704de36b5e5447f2c84cfb562f756196fcddea4
Vaysha-Yawn <Viktoriyakokorina2001@gmail.com>
2022-08-01 22:19:19 +0700

    Customized text colors and styles

--- ProjectFilmo/app/src/main/java/com/example/filmo/MainVM.kt
+++ ProjectFilmo/app/src/main/java/com/example/filmo/MainVM.kt
@@ -1,7 +1,8 @@
 package com.example.filmo
 
+import android.graphics.Typeface
+import androidx.compose.ui.text.font.FontFamily
 import androidx.lifecycle.ViewModel
 
 class MainVM:ViewModel() {
-
 }
--- ProjectFilmo/app/src/main/java/com/example/filmo/ui/theme/Type.kt
+++ ProjectFilmo/app/src/main/java/com/example/filmo/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.example.filmo.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -8,11 +9,44 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    // для навания фильма в его описании
+    h1 = TextStyle(
+        color = Color.White,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+    ),
+
+    // подзаголовок в описании фильма
+    subtitle1 = TextStyle(
+        color = Color.DarkGray,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    // для навания подборки
+    subtitle2 = TextStyle(
+        color = Color.Gray,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    // для навания фильма в его описании
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+        fontSize = 14.sp
+    ),
+
+    // для имени актера
+    h4 = TextStyle(
+        fontSize = 10.sp
+    ),
+
+    // для роли актера
+    h5 = TextStyle(
+        color = Color.Gray,
+        fontSize = 8.sp
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
